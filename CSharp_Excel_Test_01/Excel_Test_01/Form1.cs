09dd44a00ede8828275a33a5a59a688ad86d2031
Everton Nascimento  - 261 <everton.nascimento@Bx.PCEBG.COM>
2016-12-22 08:01:43 -0200

    CSharp Samples with WebCam

---
+++ CSharp_Excel_Test_01/Excel_Test_01/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Excel_Test_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application(); // Verifica se o Excel está instalado.
+
+            if (xlApp == null)
+            {
+                MessageBox.Show("Excel is not Properly Installed!!!");
+                return;
+            }
+
+            Excel.Workbook xlWorkbook;
+            Excel.Worksheet xlWorksheet;
+            object misValue = System.Reflection.Missing.Value;
+
+            xlWorkbook = xlApp.Workbooks.Add(misValue);
+            xlWorksheet = (Excel.Worksheet)xlWorkbook.Worksheets.get_Item(1);
+            xlWorksheet.Cells[1, 1] = "Sheet 1 content";
+
+            xlWorkbook.SaveAs("D:\\ExcelCSharpText.xlsx", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkbook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            releaseObject(xlWorksheet);
+            releaseObject(xlWorkbook);
+            releaseObject(xlApp);
+
+            MessageBox.Show("Excel file created, you can find the file D:\\");
+
+        }
+
+        private void releaseObject(object obj)
+        {
+
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+
+                obj = null;
+                MessageBox.Show("Exception Occured while releasing Object" + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
---
+++ CSharp_Var_and_DataTypes_Sample/CSharp_Var_and_DataTypes_Sample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp_Var_and_DataTypes_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string city;
+            sbyte age;
+            int pin;
+
+            Console.WriteLine("Enter your name:\n");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter your City:\n");
+            city = Console.ReadLine();
+
+            Console.WriteLine("Enter your Age:\n");
+            age = sbyte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your pin:\n");
+            pin = Int32.Parse(Console.ReadLine());
+
+            //Mostrando a mensagem na tela
+            Console.WriteLine("=================");
+            Console.WriteLine("Your complete Address: ");
+            Console.WriteLine("=================\n");
+
+            Console.WriteLine("Name = {0}", name);
+            Console.WriteLine("City = {0}", city);
+            Console.WriteLine("Age = {0}", age);
+            Console.WriteLine("Pin = {0}", pin);
+            Console.WriteLine("=================\n");
+
+            Console.ReadLine();
+        }
+    }
+}
